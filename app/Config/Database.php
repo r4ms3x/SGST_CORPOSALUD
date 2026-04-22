b5e83c719411a8a39f39e05e4f5c8b2f5cbda788
Maria <maitamaria147@gmail.com>
2026-04-22 08:53:03 -0400

    database_cambios

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Config;
-
+ 
 use CodeIgniter\Database\Config;
 
 /**
@@ -24,32 +24,26 @@ class Database extends Config
      *
      * @var array<string, mixed>
      */
-    public array $default = [
-        'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
-        'DBDriver'     => 'MySQLi',
-        'DBPrefix'     => '',
-        'pConnect'     => false,
-        'DBDebug'      => true,
-        'charset'      => 'utf8mb4',
-        'DBCollat'     => 'utf8mb4_general_ci',
-        'swapPre'      => '',
-        'encrypt'      => false,
-        'compress'     => false,
-        'strictOn'     => false,
-        'failover'     => [],
-        'port'         => 3306,
-        'numberNative' => false,
-        'foundRows'    => false,
-        'dateFormat'   => [
-            'date'     => 'Y-m-d',
-            'datetime' => 'Y-m-d H:i:s',
-            'time'     => 'H:i:s',
-        ],
-    ];
+
+   public $default = [
+    'DSN'      => '',
+    'hostname' => 'localhost',
+    'username' => 'postgres',
+    'password' => '',
+    'database' => '',
+    'DBDriver' => 'PostgreSQL',
+    'DBPrefix' => '',
+    'pConnect' => false,
+    'DBDebug'  => (ENVIRONMENT !== 'production'),
+    'charset'  => 'utf8',
+    'DBCollat' => 'utf8_general_ci',
+    'swapPre'  => '',
+    'encrypt'  => false,
+    'compress' => false,
+    'strictOn' => false,
+    'failover' => [],
+    'port'     => 5432,
+];
 
     //    /**
     //     * Sample database connection for SQLite3.

6d15bdd54b5c11c583768f9fc782d120fca176e9
r4ms3x <ramsesbarreto82@gmail.com>
2026-04-25 20:05:02 -0300

    agregado_dashboard_usuario

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,6 @@ class Home extends BaseController
 {
     public function index()
     {
-        // Esta es la ruta a la vista que creaste en app/Views/usuario/dashboard.php
-        return view('admin/gestion_tec');
+        return view('usuario/dashboard');
     }
 }
